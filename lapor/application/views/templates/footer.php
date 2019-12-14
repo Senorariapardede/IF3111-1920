0016166a3badae489998cd53fe9e0e65ced7d75e
14117096_PABW-RA <58059416+Yosi096@users.noreply.github.com>
2019-12-14 23:53:36 +0700

    menambahkan tempate fitur

---
+++ lapor/application/views/templates/footer.php
@@ -0,0 +1,8 @@
+    <body>
+        <div id = "footer">
+                Copyright &copy; 2019
+                Design by 14117022 14117115 14117096
+            </div>
+        </div>
+    </body>
+</html>
