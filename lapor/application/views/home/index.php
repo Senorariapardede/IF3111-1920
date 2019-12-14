7f39de283933be19c24c3a40070be07a13d87381
putrimaulidyay <54922606+putrimaulidyay@users.noreply.github.com>
2019-12-14 23:24:50 +0700

    menambahkan file index.php di folder views/home

--- lapor/application/views/home/index.php
+++ lapor/application/views/home/index.php
@@ -1,8 +1,49 @@
 <html>
-<head>
-<title>Home/index</title>
-</head>
-<body>
-<h1>Home/index</h1>
-</body>
+    <head>
+        <title><?php echo $title;?></title>
+        <link rel="stylesheet" type="text/css" href="other/css/style.css>v=1.1">
+        <script src="other/val/validation.js"></script>
+    </head>
+    <body>
+        <div class="utama">
+            <form class="search1" onSubmit="return search(this)" action="<?php echo base_url('search')?>" method="post">
+                <input class="search2" name="key" placeholder="" autocomplete="off" type="text" autofocus>
+                <input class="button" name="submit" placeholder="search" value="Cari" type="submit">
+            </form>
+
+            <a class="add" href="<?php base_url();?>Add"><b>Buat Laporan/Komentar</b></a>
+            <p calss="jud">Laporan Terakhir</p>
+            <br><br>
+
+            <?php foreach ($home as $a):?>
+                <?php $id = $a['id']; ?>
+                <?php $today = date("Y-m-d H:i"); ?>
+
+            <div class="wh">
+                <p><?php echo substr($a['laporan'], 0, 500); ?></p>
+            </div><br>
+
+                <?php
+                   echo "<tr>
+                   <a class='tm' href='detail?detail_id=$id'>Lihat Selengkapnya</a></tr>";
+                ?>
+
+            <div class="time">
+                <?php $data=new DateTime($a['waktu']);
+                   echo 'Waktu : ';
+                   echo $date->format('d-m-Y H:i');?>
+            </div>
+
+            <div class="lampir">
+                <?php echo 'Lampiran : ';
+                   echo $a['lampiran']; ?>
+            </div>
+            <br><br>
+
+            <?php endforeach; ?>
+            <img class="img" src="<?php echo base_url();?>other/gmbr/titik.jpg">
+            <img class="img" src="<?php echo base_url();?>other/gmbr/titik.jpg">
+        <br>
+        </div>
+    </body>
 </html>
