16ec478e575677cb43d71bd163814532ea793f49
putrimaulidyay <54922606+putrimaulidyay@users.noreply.github.com>
2019-12-14 23:40:44 +0700

    update file index.php di folder vies/V_news

--- lapor/application/views/V_news/index.php
+++ lapor/application/views/V_news/index.php
@@ -2,7 +2,7 @@
     <head></head>
     <body>
         <?php foreach ($news as $ShowNews){?>
-        <?php echo site_url('news/'.$ShowNews['laporan']);?></a><h1>
+        <?php echo site_url('news/'.$ShowNews['laporan']);?>
             <p><?php echo substr($ShowNews['laporan'], 0, 10); ?></p>
             <p><a href="<?php echo site_url('news/'.$ShowNews['laporan']); ?>">Selengkapnya</a></p>
         <?php } ?>    
